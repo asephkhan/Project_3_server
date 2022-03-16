a48eb3139901f2228e7ff586be46fa84cb7be04b
Asif Ali <asephkhan@gmail.com>
2022-03-16 16:01:17 +0000

    navbar

--- routes/trip.routes.js
+++ routes/trip.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Trip = require('../models/Trip.model') 
-/* const Flight = require('../models/Flight.model');  */
 const mongoose = require('mongoose');
 
 
